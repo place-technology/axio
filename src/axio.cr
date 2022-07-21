f1a24abb703a420b6e568d8ba52c4fdf3daf9d47
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2022-07-21 08:32:52 +0400

    Update axio.cr

--- src/axio.cr
+++ src/axio.cr
@@ -3,10 +3,3 @@ require "./axio/**"
 
 module Axio
 end
-
-Log.setup(:debug)
-
-client = Axio::Client.new(base_url: "http://130.113.46.137:60001", username: "axiomtest1", password: "UpTheTree1!")
-
-client.access_points.unlock(id: "2127", permanent: "true")
-client.access_points.lock(id: "2127", permanent: "true")
