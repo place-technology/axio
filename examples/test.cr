2b3599346fca385e981c845a14671a457f2c297f
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2022-07-27 14:18:57 +0400

    Change the host name to use a proper one

--- examples/test.cr
+++ examples/test.cr
@@ -2,7 +2,11 @@ require "../src/axio"
 
 Log.setup(:debug)
 
-client = Axio::Client.new(base_url: "http://127.0.0.1:60001", username: "test", password: "test")
+client = Axio::Client.new(base_url: "http://130.113.46.137:60001", username: "axiomtest1", password: "UpTheTree1!")
 
-client.access_points.unlock(id: "1", permanent: "true")
-client.access_points.lock(id: "1", permanent: "true")
+client.access_points.unlock(id: "2127", permanent: "true")
+client.access_points.lock(id: "2127", permanent: "true")
+pp client.access_points.status(id: "2127").body
+
+response = client.try(&.access_points.status(id: "54017"))
+pp JSON.parse(response.body)
